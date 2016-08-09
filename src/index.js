aec3b83c330968912f6997f15b32f8ba2766f80a
Sean Rose <sean1rose@gmail.com>
2016-08-09 00:33:50 -0700

    Fix linting and create search component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<p>Hi</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-	<App />,
-	document.getElementById('container')
-)
+  <App />,
+  document.getElementById('container')
+);
