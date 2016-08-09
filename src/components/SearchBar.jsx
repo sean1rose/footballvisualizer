7219f9671500256f4975f796401cdd342458d12a
Sean Rose <sean1rose@gmail.com>
2016-08-09 01:04:01 -0700

    Set up search component event handler

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  // declare constructor func, pass it the component's props
+  constructor(props) {
+    // call SearchBar's parent constructor via the super call
+    // (if don't call super, then this will be undefined)...
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    // need to manage this change in state
+    this.setState({
+      searchTerm: event.target.value,
+    });
+  }
+
   render() {
-    return <input />;
+    return <input onChange={this.handleInputChange} />;
   }
 }
 
